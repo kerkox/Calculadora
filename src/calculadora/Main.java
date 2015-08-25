34a763555f80d00f3bbbaee6245bf631638f113d
Kerkox <bryanpoolker@gmail.com>
2015-08-24 23:33:52 -0500

    Version 0.0.1

---
+++ src/calculadora/Main.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+/**
+ *
+ * @author Polker
+ */
+public class Main {
+
+   
+    public static void main(String[] args) {
+      
+        Calculadora cal = new Calculadora();
+        cal.setVisible(true);
+    }
+    
+}
